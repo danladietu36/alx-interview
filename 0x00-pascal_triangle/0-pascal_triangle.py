f5a20b1760977ae4b545922c1630cb130145a29c
Danladi Etu <danladietugithub@example.com>
2023-07-27 09:55:58 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,12 +11,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangles = [[1]]
+    triangle = [[1]]
     while len(triangles) != n:
         tri = triangles[-1]
         tmp = [1]
         for i in range(len(tri) - 1):
             tmp.append(tri[i] + tri[i + 1])
         tmp.append(1)
-        triangles.append(tmp)
-    return triangles
+        triangle.append(tmp)
+    return triangle
