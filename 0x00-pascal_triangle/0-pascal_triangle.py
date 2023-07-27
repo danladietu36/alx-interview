5fc343bfabba290eed3164ea707bd9542bbee637
Danladi Etu <danladietugithub@example.com>
2023-07-27 09:24:41 +0000

    added

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/python3
+
+def pascal_triangle(n):
+    """
+    A pascal triangle that returns a list of integers representing
+    the Pascal's triangle of n.
+    """
+
+    triangle = []
+
+    if (n <= 0):
+        return triangle
+
+    for x in range(n):
+        tmp_list = []
+
+        for y in range(x + 1):
+            if y == 0 or y == x:
+                tmp_list.append(1)
+            else:
+                tmp_list.append(triangle[x-1][y-1] + triangle[x-1][y])
+        triangle.append(tmp_list)
+    return (traingle)
