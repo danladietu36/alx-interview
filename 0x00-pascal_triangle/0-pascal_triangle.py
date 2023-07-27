b08a8c26b77b1798ffd52974433b38119357da29
Danladi Etu <danladietugithub@example.com>
2023-07-27 09:54:37 +0000

    add

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,18 +8,15 @@ def pascal_triangle(n):
     A pascal triangle that returns a list of integers representing
     the Pascal's triangle of n.
     """
-
-    triangle = []
-
     if n <= 0:
-        return triangle
-    for x in range(n):
-        tmp_list = []
+        return []
 
-        for y in range(x+1):
-            if y == 0 or y == x:
-                tmp_list.append(1)
-            else:
-                tmp_list.append(triangle[x-1][y-1] + triangle[x-1][y])
-        triangle.append(tmp_list)
-    return (traingle)
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
