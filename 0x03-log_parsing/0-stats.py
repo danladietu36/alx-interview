7354fe82be84d2a561e894ea1544a10d4a9413ef
Danladi Etu <danladietugithub@example.com>
2023-08-17 23:06:49 +0000

    log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,32 +7,36 @@
 import sys
 
 
-def log_parsing():
-    """
-       Function that reads stdin line by line and
-       computes metrics.
-    """
-    cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-             '403': 0, '404': 0, '405': 0, '500': 0}
-    total_size = 0
-    count = 0
+import sys
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+count = 0
+
+try:
     for line in sys.stdin:
         line_list = line.split(" ")
-        if len(line_list) < 5:
-            return False
         if len(line_list) == 5:
             code = line_list[-2]
             size = int(line_list[-1])
             if code in cache.keys():
-                total_size += size
-                count += count
+                cache[code] += 1
+            total_size += size
+            count += 1
 
         if count == 10:
-            counter = 0
+            count = 0
             print('File size: {}'.format(total_size))
             for key, value in sorted(cache.items()):
-                if value == 0:
-                    return False
-                else:
+                if value != 0:
                     print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
