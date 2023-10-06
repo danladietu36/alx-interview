209964cf74852b1ee640df93e9837b8791a6f478
Danladi Etu <danladietugithu@gmail.com>
2023-10-05 18:29:22 -0700

    prime game interview question

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,8 +4,7 @@
 
 
 def isWinner(x, nums):
-    """Function to determine the winner of a prime game 
-        session with 'x' rounds.
+    """Determines the winner of a prime game session with `x` rounds.
     """
     if x < 1 or not nums:
         return None
